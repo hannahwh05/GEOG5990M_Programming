75dc6e467685014a3a10766fee5c5a6f17c3420e
Hannah Wheldon <43998956+hannahwh05@users.noreply.github.com>
2018-12-13 17:50:28 +0000

    update

--- ABM_Final/ABM_Final.py
+++ ABM_Final/ABM_Final.py
@@ -1,4 +1,5 @@
 '''
+GEOG5990M Programming for Geographical Information Analysis: Core Skills
 Agent Based Model
 
 201284811 Hannah Wheldon
@@ -11,26 +12,25 @@ When this code is run, a window will will appear on the computer screen called
 Agent Based Model. To run the model, click "Run model" from the menu bar in 
 this window.
 
-***Link to point below
-    n.b. to run model with random coordinates instead of scraping from 
-    webpage, remove 3rd and 4th arguements above i.e. y, x
+n.b. to run model with random coordinates instead of scraping from webpage, 
+remove 3rd and 4th arguements **********************i.e. y, x
 
 In Spyder set Tools > Preferences > Ipython console > Graphics > Set backend 
 to inline
 
-use agentframework.py for agent parameters
+Use agentframework.py for agent class
 '''
 
 ###############################################################################
 ################################# Import ######################################
-
+###############################################################################
 '''import libraries/functions/packages at top of code'''
 
 #2D plotting library
 import matplotlib
-import matplotlib.pyplot
+import matplotlib.pyplot as plot
 #animate plot
-import matplotlib.animation
+import matplotlib.animation as ani
 #framework to build tkinter graphics interface
 import tkinter
 matplotlib.use('TkAgg')
@@ -46,18 +46,17 @@ import csv
 import requests
 #pulls data from HTML (beautiful soup 4)
 import bs4
-from PIL import ImageGrab
-###############################################################################
 
-'''assign value to variables'''
+###############################################################################
+####################'''Step 1: Assign value to variables'''####################
+###############################################################################
 
 num_of_agents = 25
 neighbourhood = 75
 
 ###############################################################################
-############################ Import environment ###############################
+#######################'''Step 2: Import environment'''########################
 ###############################################################################
-
 '''read in raster data from csv'''
 
 f = open('in.txt', newline='') 
@@ -77,20 +76,17 @@ for row in reader:
 #Close the reader    				
 f.close()
 
-#len(environment) # test to see size of environment based on csv file
+#len(env) # test to see size of environment based on csv file
 
 #test to see environment alone
-matplotlib.pyplot.imshow(environment)
-matplotlib.pyplot.show()
-###############################################################################
-
-
-
+plot.imshow(environment)
+plot.show()
 
 ###############################################################################
-############### Create agents using coordinates from webpage ##################
+###########'''Step 3: Create agents using coordinates from webpage'''##########
 ###############################################################################
-#scrape a web page to get the agent starting locations
+'''scrape a web page to get the agent starting locations'''
+
 # beautiful soup 4 pulls data from html
 r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/'
                  'python/agent-framework/part9/data.html')
@@ -103,44 +99,42 @@ td_xs = soup.find_all(attrs={"class" : "x"})
 #print(td_xs) #test to see x coordinates are read in
 
 ###############################################################################
+##########################'''Step 4: Plot agents'''############################
+###############################################################################
 
-#set up container for agents
+# set up container for agents
 agents = []
 
-'''for loop to append y and x classes read in above to each agents 
-y and x coordinates'''
-
+# for loop to append x and y classes, read in above, to each agent
 for i in range(num_of_agents):
     y = int(td_ys[i].text)
     x = int(td_xs[i].text)
-    agents.append(agentframework.Agent(environment, agents))
-    #n.b. to run model with random coordinates instead of scraping from webpage,
-    # remove 3rd and 4th arguements above i.e. y, x
+    agents.append(agentframework.Agent(environment, agents, y, x))
+    #n.b. to run model with random coordinates, instead of scraping from 
+    # webpage, remove 3rd and 4th arguements above i.e. y, x
     
-#set up figure size and axes
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
+# set up figure size and axes
+fig = matplotlib.pyplot.figure(figsize=(8, 8))
 ax = fig.add_axes([0, 0, 1, 1])
 
 carry_on = True
 
 def update(frame_number):
-    """ hkm
-    
     """
-    
+    Updates the display in the animation:
+    Plot axes based on size of environment.
+    Agents move, eat, share with neighbours and vomit.
+    """
     #clear previous display           
     fig.clear()
     #create global variable to modify local variable outside of function
     global carry_on
     
-    #make plot 300 x 300 due to size of environment
-    matplotlib.pyplot.ylim(300, 0)
-    matplotlib.pyplot.xlim(0, 300)
-    
+    #make plot based on size of environment
+    plot.xlim(0, len(environment))
+    plot.ylim(0, len(environment[0]))
 
-    #random.shuffle(agents)
-
-    #for each agent - move, eat and share with neighbours 
+    #for each agent - move, eat, share with neighbours and vomit 
     for i in range(num_of_agents):
         #agents randomly move around environment
         agents[i].move()
@@ -150,56 +144,62 @@ def update(frame_number):
         agents[i].vomit()
         #for loop to plot all agents generated
         
-    #plot environment and set minimum and maximum limits
-    matplotlib.pyplot.imshow(environment, vmin = 0, vmax= 300)
+    #plot environment and set minimum and maximum limits based on environment
+    # values to prevent scaling of colour when model runs
+    plot.imshow(environment, vmin = 92, vmax= 255)
         
     for i in range(num_of_agents):
-        matplotlib.pyplot.scatter(agents[i]._x, agents[i]._y)
-    
-    #if random move is less than 0.01 stopping condition is met     
+        plot.scatter(agents[i]._x, agents[i]._y)
+  
+###############################################################################
+######################'''Step 4: Stopping condition'''#########################
+###############################################################################
+
+#if random number generated is less than 0.01 stopping condition is met     
     if random.random() < 0.01:
         carry_on = False
         print("stopping condition")    
     
-#generator function to set condition for when to stop 
+# generator function to set condition for when to stop 
 def gen_function(b = [0]):
+    """A stopping function for the animation"""
     a = 0
     global carry_on
-    #a = number of iterations
-    while (a < 100) & (carry_on) :
+    while (a < 100) & (carry_on):
         #function returns generator
         yield a			
         a = a + 1
+        
+###############################################################################
+#########################'''Step 5: Run the model'''###########################
+############################################################################### 
 
-#define run for model to animate plot for GUI interface
 def run():
+    """ Run model to animate plot for GUI interface"""
     global animation
-    animation = matplotlib.animation.FuncAnimation(fig, update, 
+    animation = ani.FuncAnimation(fig, update, 
                 frames=gen_function, repeat=False)
     canvas.show()
     
 ###############################################################################
-########################## GUI Interface ######################################
+#########################'''Step 6: GUI Interface'''###########################
+###############################################################################
+'''use tkinter to make GUI interface and widgets'''
 
-#use tkinter to make GUI interface and widgets
 root = tkinter.Tk() 
 root.wm_title("Agent Based Model")
 
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
+    
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 model_menu = tkinter.Menu(menu_bar)
-menu_bar.add_cascade(label="Model Menu", menu=model_menu)
-model_menu.add_command(label="Run model", command=run)
+menu_bar.add_cascade(label="Menu", menu=model_menu)
+model_menu.add_command(label="Run", command=run)
 model_menu.add_command(label="Close", command=root.destroy)
 
-#model_menu.add_command(label="Save as...", command=tkinter.Image.('ABM.jpg'))
-
 tkinter.mainloop()
-
-
 
 ###############################################################################
 
--- ABM_Final/agentframework.py
+++ ABM_Final/agentframework.py
@@ -1,4 +1,5 @@
 '''
+GEOG5990M Programming for Geographical Information Analysis: Core Skills
 Agent Framework for ABM_Final
 
 This file contains the code for the Agent class. This is imported into the
@@ -17,52 +18,78 @@ import random
 # Create a new type of object with attributes attached
     # Create own functions, using 'def'
     
-class Agent (object):
+class Agent():
+    """
+    Agent class:
+    A class to show the behaviour of an abstract agent, e.g. a sheep, 
+    interacting with an environment.
+    Agent characteristics:
+        - store
+        - x-coordinate
+        - y-coordinate
+    Agent behaviours include:
+        - move
+        - eat
+        - share with neighbours
+        - vomit
+    """
     # Constructor to pass arguments into to create Agent
-    def __init__(self, environment, agents, y = None, x = None):
-        self.environment = environment
+    def __init__(self, env, agents, x=None, y=None):
+        """
+        Constructor takes arguments:
+        env -- a list (environment) of lists (rowlists), imported from a csv 
+               file, creating the 2D landscape in which the agent exists.
+        agents -- all the agents in the environment.
+        x -- the x axis coordinate.
+        y -- the y axis coordinate.
+        """
+        self.environment = env
         #Make Agent aware of the other agents
         self.agents = agents
         self.store = 0
-        if (x == None):
-           self._x = random.randint(0,300)
-        else:
-            self._x = x   
-        if (y == None):
-            self._y = random.randint(0,300)
-        else:
-            self._y = y
-    
-    # Move the agent within the environment
-    
-    # Agent moves +1 space if less than 0.5 otherwise -1 space
-    # % keeps the agents within the environment (300x300)
-    
+        self._envWidth = len(env) 
+        self._envHeight = len(env[0])
+        self._x = random.randint(0, self._envWidth)
+        self._y = random.randint(0, self._envHeight)
+
+
     def move(self):
-        #y coordinate
+        """
+        Movement of agent within the environment.
+        Agent moves along x and y axis dependent on a random number generated.
+        Moves +1 space if random number is less than 0.5, otherwise -1 
+        space if more than 0.5 (random number is between 0.0-1.0).
+        "%" operator keeps the agents within the environment.       
+        """
+        # Move x
         if random.random() < 0.5:
-            self._y = (self._y + 1) % 300
+            self._x = (self._x + 1) % self._envWidth
         else:
-            self._y = (self._y - 1) % 300
-        #x coordinate
+            self._x = (self._x - 1) % self._envWidth
+        # Move y
         if random.random() < 0.5:
-            self._x = (self._x + 1) % 300
+            self._y = (self._y + 1) % self._envHeight
         else:
-            self._x = (self._x - 1) % 300
-    
-    # Agent eats from environment if it has more than 10 stores 
-    # Agents eat what is left
-    
+            self._y = (self._y - 1) % self._envHeight
+        
+     
     def eat(self):
+        """
+        Eating habit of agent.
+        Agent eats 10 "stores" from environment if it has more than 10.
+        """
         if self.environment[self._x][self._y] > 10:
             self.environment[self._x][self._y] -= 10
             self.store += 10   
     
-    # Agents have neighbours which they share their food stores with if they
-    # are in the distance of the neighbourhood. In this case, Agents average
-    # out their stores.
     
     def share_with_neighbours(self, neighbourhood):
+        """
+        Agents have neighbours which they share their food stores with if they
+        are in the distance of the neighbourhood. In this case, agents average
+        out their stores.
+        neighbourhood -- the distance at which an agent will share with another
+        """
         for agent in self.agents:
             # Calculate the distance between self and the current other agent:
             dist = self.distance_between(agent) 
@@ -72,21 +99,24 @@ class Agent (object):
                 ave = sum /2
                 self.store = ave
                 agent.store = ave
-                # print("sharing " + str(dist) + " " + str(ave)) #test
-                # print(self.store)
+                #print("sharing " + str(dist) + " " + str(ave)) #test
+                #print(self.store)
     
-    # If Agent reaches store capacity of more than 300, Agent will vomit 300
-    # of stores
     
     def vomit(self):
-        if self.store >=300:
-            self.environment[self._x][self._y] +=300
-            self.store -= 300
-            #print("vomited here") # test to see if vomitted
+        """
+        Agent vomits if it reaches store capacity of 255 (maximum value of the 
+        environment.
+        """
+        if self.store >=255:
+            self.environment[self._x][self._y] +=255
+            self.store -=255
+            #print("vomited here") # test to see if vomited
     
-    # This function finds the distance between row a and row b using 
-    # pythagoras theorum while using the agent class
     
     def distance_between(self, agents_row_b):
+        """
+        Returns the distance between row a and row b using Pythagoras theorum.
+        """
         return (((self._x - agents_row_b._x)**2) +
                 ((self._y - agents_row_b._y)**2))**0.5
